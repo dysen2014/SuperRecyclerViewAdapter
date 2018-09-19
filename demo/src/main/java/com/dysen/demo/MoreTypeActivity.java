585c235eb56ee0a954f52e4c3a1256d015890b5e
dysen2018 <dysen@outlook.com>
2018-09-19 17:02:50 +0800

    日常提交。

--- demo/src/main/java/com/dysen/demo/MoreTypeActivity.java
+++ demo/src/main/java/com/dysen/demo/MoreTypeActivity.java
@@ -6,10 +6,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.dysen.library.SuperRecyclerAdapter;
 import com.dysen.library.SuperRecyclerHolder;
+import com.dysen.library.SyncHorizontalScrollView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -17,7 +21,7 @@ import java.util.List;
 
 /**
  * 多类型
- *
+ * <p>
  * Created by dysen on 2018/9/19.
  */
 public class MoreTypeActivity extends AppCompatActivity {
@@ -27,7 +31,8 @@ public class MoreTypeActivity extends AppCompatActivity {
     private SuperRecyclerAdapter<Person> mAdapter;
     private List<Person> mList = new ArrayList<>();
 
-    @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_common_recycler);
         initView();
@@ -40,8 +45,45 @@ public class MoreTypeActivity extends AppCompatActivity {
 
         mAdapter = new SuperRecyclerAdapter<Person>(this, mList) {
             @Override
+            public void convert(SuperRecyclerHolder holder, final Person person, int layoutType,
+                                final int position) {
+                TextView vv = (TextView) holder.getItemView().findViewById(R.id.vv);
+                vv.setText("==========================================");
+                //只有一种布局，不使用layoutType来区分type了
+                holder//
+                        .setText(Arrays.asList(new Integer[]{R.id.tv_name, R.id.tv_addr, R.id.tv_age, R.id.tv_phone}),
+                                Arrays.asList(new String[]{person.getName(), person.getAddr(), String.valueOf(person.getAge()), String.valueOf(person.getNumber())}), "--")
+                        .setOnItemClickListenner(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Toast.makeText(MoreTypeActivity.this, "item被点击" + position, Toast.LENGTH_SHORT)
+                                        .show();
+                            }
+                        });
+            }
+
+            @Override
+            public int getLayoutAsViewType(Person s, int position) {
+                return R.layout.recycler_item_more;
+            }
+        };
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mRv.setLayoutManager(layoutManager);
+//        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2);
+//        mRv.setLayoutManager(gridLayoutManager);
+        mRv.setAdapter(mAdapter);
+    }
+
+    private void initView2() {
+
+        mRv = (RecyclerView) findViewById(R.id.rv);
+
+        mAdapter = new SuperRecyclerAdapter<Person>(this, mList) {
+            @Override
             public void convert(SuperRecyclerHolder holder, final Person p, int layoutType,
-                final int position) {
+                                final int position) {
                 //只有一种布局，不使用layoutType来区分type了
                 holder//
                         .setText(Arrays.asList(new Integer[]{R.id.tv_name, R.id.tv_addr, R.id.tv_age, R.id.tv_phone}),
@@ -50,15 +92,17 @@ public class MoreTypeActivity extends AppCompatActivity {
 //                    .setText(R.id.tv_addr, p.getAddr())
 //                    .setText(R.id.tv_age, p.getAge())
 //                    .setText(R.id.tv_phone, p.getPhone())
-                    .setOnItemClickListenner(new View.OnClickListener() {
-                        @Override public void onClick(View v) {
-                            Toast.makeText(MoreTypeActivity.this, "item被点击"+position, Toast.LENGTH_SHORT)
-                                .show();
-                        }
-                    });
+                        .setOnItemClickListenner(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Toast.makeText(MoreTypeActivity.this, "item被点击" + position, Toast.LENGTH_SHORT)
+                                        .show();
+                            }
+                        });
             }
 
-            @Override public int getLayoutAsViewType(Person s, int position) {
+            @Override
+            public int getLayoutAsViewType(Person s, int position) {
                 return R.layout.recycler_item_more;
             }
         };
@@ -73,9 +117,9 @@ public class MoreTypeActivity extends AppCompatActivity {
 
     private void initData() {
         for (int i = 1; i <= 100; i++) {
-            if (i%10 == 0)
-            mList.add(new Person("", null, i+10, i));
-            mList.add(new Person("张三"+i, "长安街"+i+"号", i+10, i));
+            if (i % 10 == 0)
+                mList.add(new Person("", null, -1, -1));
+            mList.add(new Person("张三" + i, "中国北京的长安街" + i + "号", i + 10, i));
         }
         mAdapter.notifyDataSetChanged();
     }
